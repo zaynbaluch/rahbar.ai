bef13c28aec4a7e17eebacc56f45b3c67bbde729
MuhammadMusaab-UlHaq <mhaq.bscs24seecs@seecs.edu.pk>
2026-09-02 17:57:47 +0500

    ui: persist workflow and recent context

---
+++ app/test/features/curriculum/recent_work_store_test.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:bayaz_ai/features/curriculum/recent_work_store.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late Directory temp;
+  late File file;
+  setUp(() async {
+    temp = await Directory.systemTemp.createTemp('recent-work-');
+    file = File('${temp.path}/recent.json');
+  });
+  tearDown(() async => temp.delete(recursive: true));
+
+  test('recent reference persists and can be cleared', () async {
+    final store = RecentWorkStore(fileProvider: () async => file);
+    await store.update(const RecentWorkReference(type: 'lesson', id: 'lesson-1'));
+    final reopened = RecentWorkStore(fileProvider: () async => file);
+    expect((await reopened.current())?.id, 'lesson-1');
+    await reopened.clear();
+    expect(await reopened.current(), isNull);
+  });
+}
